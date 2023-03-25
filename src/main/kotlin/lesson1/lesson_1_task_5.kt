698811b9076c91b570887f558d43aa182edb2aaf
summerbreeze11 <wf992211@mail.ru>
2023-03-26 01:14:38 +0700

    KS-1-5 added made transfer transactions

--- src/main/kotlin/lesson1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson1/lesson_1_task_5.kt
@@ -4,6 +4,6 @@ fun main(){
     val second = 6480
     val minute = second/60
 
-    println("[$minute:${String.format("%02.0f", second - minute*60F)}]")
+    println("$minute:${String.format("%02.0f", second - minute*60F)}")
 
 }
