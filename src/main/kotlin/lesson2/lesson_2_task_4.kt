6519c6328d38cf118d3fa2be87ba5d64a38a578f
summerbreeze11 <wf992211@mail.ru>
2023-03-27 18:38:24 +0700

    KS-2-4 correct solution

--- src/main/kotlin/lesson2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson2/lesson_2_task_4.kt
@@ -4,6 +4,6 @@ fun main() {
     val crystalOre = 7
     val ironOre = 11
 
-    println("Crystal ore buff: ${(crystalOre * 1.2 - crystalOre).toInt()}")
-    println("Iron ore buff: ${(ironOre * 1.2 - ironOre).toInt()}")
+    println("Crystal ore buff: ${(crystalOre * 0.2).toInt()}")
+    println("Iron ore buff: ${(ironOre * 0.2).toInt()}")
 }
