958650fb4ea9c438996fffdfb2736ef9ce96b317
summerbreeze11 <wf992211@mail.ru>
2023-03-29 04:07:15 +0700

    KS-4-4 use DRY principle

--- src/main/kotlin/lesson4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson4/lesson_4_task_4.kt
@@ -2,13 +2,15 @@ package lesson4
 
 fun main() {
     val trainingDay = 5
+    val isEven = (trainingDay % 2 == 0)
 
     println(
         """
-        |Упражнения для рук: ${trainingDay % 2 == 1}
-        |Упражнения для ног: ${trainingDay % 2 != 1}
-        |Упражнения для спины: ${trainingDay % 2 != 1}
-        |Упражнения для преса: ${trainingDay % 2 == 1}
+        |Упражнения для рук: ${!isEven}
+        |Упражнения для ног: ${isEven}
+        |Упражнения для спины: ${isEven}
+        |Упражнения для преса: ${!isEven}
        """.trimMargin()
     )
+
 }
