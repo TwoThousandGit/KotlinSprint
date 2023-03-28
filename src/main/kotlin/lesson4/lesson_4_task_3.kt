70b60e76f3fbaeec91a16789565371b9cbd73d28
summerbreeze11 <wf992211@mail.ru>
2023-03-29 03:50:58 +0700

    KS-4-3 add boolean flag

--- src/main/kotlin/lesson4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson4/lesson_4_task_3.kt
@@ -1,14 +1,14 @@
 package lesson4
 
 fun main() {
-    val weatherToday = "солнечно"
-    val awningCondition = "тент раскрыт"
+    val goodWeatherToday = true
+    val isTendOpen = true
     val moisture = 20
-    val season = "зима"
+    val isWinter = true
 
     println(
         "Благоприятные ли условия сейчас для роста бобовых? ${
-            weatherToday == "солнечно" && awningCondition == "тент раскрыт" && moisture == 20 && season == "зима"
+            goodWeatherToday && isTendOpen && moisture == 20 && isWinter
         }"
     )
 }
