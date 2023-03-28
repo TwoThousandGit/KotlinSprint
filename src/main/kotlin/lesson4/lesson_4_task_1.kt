fb40946cda662b1f04609089876eaa2d8bce1391
summerbreeze11 <wf992211@mail.ru>
2023-03-28 22:49:04 +0700

    KS-4-1

---
+++ src/main/kotlin/lesson4/lesson_4_task_1.kt
@@ -0,0 +1,9 @@
+package lesson4
+
+fun main() {
+    val tablesTomorrow = 9
+    val tablesToday = 13
+
+    println("Доступность столиков на сегодня: ${tablesToday < 13}")
+    println("Доступность столиков на завтра: ${tablesTomorrow < 13}")
+}
