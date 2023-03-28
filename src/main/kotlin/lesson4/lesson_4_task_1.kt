e2e8ae937d840bbb00ba1cb49fdb5d355e5233ee
summerbreeze11 <wf992211@mail.ru>
2023-03-29 03:20:31 +0700

    KS-4-1 delete magic number

--- src/main/kotlin/lesson4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson4/lesson_4_task_1.kt
@@ -1,9 +1,11 @@
 package lesson4
 
+const val MAX_TABLE_SIZE = 13
+
 fun main() {
     val tablesTomorrow = 9
     val tablesToday = 13
 
-    println("Доступность столиков на сегодня: ${tablesToday < 13}")
-    println("Доступность столиков на завтра: ${tablesTomorrow < 13}")
+    println("Доступность столиков на сегодня: ${tablesToday < MAX_TABLE_SIZE}")
+    println("Доступность столиков на завтра: ${tablesTomorrow < MAX_TABLE_SIZE}")
 }
