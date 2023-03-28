37713ccab469d586c508ba1c8a29fedd16642509
summerbreeze11 <wf992211@mail.ru>
2023-03-28 21:24:18 +0700

    KS-3-2

---
+++ src/main/kotlin/lesson3/lesson_3_task_2.kt
@@ -0,0 +1,13 @@
+package lesson3
+
+fun main() {
+    val name = "Татьяна"
+    var secondname = "Андреева"
+    val thirdname = "Сергеевна"
+    var age = 20
+    println("$secondname $name $thirdname, возраст: $age")
+
+    secondname = "Сидорова"
+    age = 22
+    println("$secondname $name $thirdname, возраст: $age")
+}
