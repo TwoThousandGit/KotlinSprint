95e3011c0bab203f010e2869706b5c6fa4052d4f
summerbreeze11 <wf992211@mail.ru>
2023-03-28 23:02:03 +0700

    KS-4-2

--- src/main/kotlin/lesson4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson4/lesson_4_task_2.kt
@@ -5,7 +5,7 @@ fun main() {
     val maxWeight = 100
     val maxVolume = 99
 
-    println("Average для груза с весом 20 кг:${20 <= maxWeight && 20>=minWeight}")
-    println("Average для груза с весом 50 кг и объемом 80 л:${50 <= maxWeight && 50>=minWeight && 80 <= maxVolume}")
+    println("Average для груза с весом 20 кг:${20 <= maxWeight && 20 >= minWeight}")
+    println("Average для груза с весом 50 кг и объемом 80 л:${50 <= maxWeight && 50 >= minWeight && 80 <= maxVolume}")
     println("Average для груза с объемом 100л:${100 < maxVolume}")
 }
