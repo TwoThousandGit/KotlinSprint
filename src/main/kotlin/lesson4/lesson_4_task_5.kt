c231100e9bd70ae9d8efcb0d6db77c1d30fa0abd
summerbreeze11 <wf992211@mail.ru>
2023-03-29 04:26:23 +0700

    KS-4-5

--- src/main/kotlin/lesson4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson4/lesson_4_task_5.kt
@@ -5,25 +5,18 @@ const val MIN_CREW = 55
 const val MIN_PROVISION = 50
 
 fun main() {
-    val damages: Boolean
-    val crewNumber: Int
-    val provisionBoxes: Int
-    val goodWeather: Boolean
-    val isReady: Boolean
-
     print("Есть ли повреждения корпуса? (true|false): ")
-    damages = readLine().toBoolean()
+    val damages = readLine().toBoolean()
     print("Введте численность экипажа: ")
-    crewNumber = readln().toInt()
+    val crewNumber = readln().toInt()
     print("Введите количество ящиков провизии: ")
-    provisionBoxes = readln().toInt()
+    val provisionBoxes = readln().toInt()
     print("Благоприятные ли метеоусловия? (true|false): ")
-    goodWeather = readLine().toBoolean()
+    val goodWeather = readLine().toBoolean()
 
-    isReady = (provisionBoxes > MIN_PROVISION) &&
+    val isReady = (provisionBoxes > MIN_PROVISION) &&
             ((!damages && (crewNumber in (MIN_CREW + 1) until MAX_CREW)) ||
                     ((crewNumber == MAX_CREW) && goodWeather))
 
     println("Корабль может отправиться в длительное плавание: $isReady")
-
 }
