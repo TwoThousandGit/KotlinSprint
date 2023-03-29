4a6650278ceb24a929f02ad368868cf3ac3e1683
summerbreeze11 <wf992211@mail.ru>
2023-03-29 18:26:37 +0700

    KS-5-1

---
+++ src/main/kotlin/lesson5/lesson_5_task_1.kt
@@ -0,0 +1,9 @@
+package lesson5
+
+fun main() {
+    println("Сколько будет 2+10?")
+    val checkResult = readln().toInt()
+
+    if (checkResult == 12) println("Добро пожаловать")
+    else println("Доступ запрещен.")
+}
