a92bf22057b778348fd2e022b1f1231e726ff6d1
summerbreeze11 <wf992211@mail.ru>
2023-03-29 18:55:34 +0700

    KS-5-3

---
+++ src/main/kotlin/lesson5/lesson_5_task_3.kt
@@ -0,0 +1,25 @@
+package lesson5
+
+fun main() {
+    val firstSecretNum = 99
+    val secondSecretNum = 11
+
+    println("\"Лотерейка\" - угадайте 2 числа от 0 до 100 и выиграйте главный приз - 1000$ ")
+    print("Попробуйте угадать первое число: ")
+    val firstUserNum = readln().toInt()
+    print("Хорошо!\nТеперь попробуйте угадать второе число: ")
+    val secondUserNum = readln().toInt()
+
+    val resultText = if (((firstUserNum == firstSecretNum) || (firstUserNum == secondSecretNum))
+        && ((secondUserNum == secondSecretNum) || (secondUserNum == firstSecretNum))
+    )
+        "Поздравляем! Вы выиграли главный приз!"
+    else if (((firstUserNum == firstSecretNum) || (firstUserNum == secondSecretNum))
+        || ((secondUserNum == secondSecretNum) || (secondUserNum == firstSecretNum))
+    )
+        "Вы выиграли утешительный приз!"
+    else
+        "Неудача! Крутите барабан!"
+
+    println(resultText)
+}
